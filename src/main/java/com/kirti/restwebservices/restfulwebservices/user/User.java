f745db09f89d00cf758ade5b0ab92cd71072e400
kirti <kirtiadikane22@gmail.com>
2023-10-11 17:46:28 +0530

    User validation and exception handler

--- src/main/java/com/kirti/restwebservices/restfulwebservices/user/User.java
+++ src/main/java/com/kirti/restwebservices/restfulwebservices/user/User.java
@@ -2,11 +2,18 @@ package com.kirti.restwebservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 //User Bean
 public class User {
 
 	private Integer id;
+	
+	@Size(min=2, message = "Name should have atleast 2 characters")
 	private String name;
+	
+	@Past(message = "Birth date should be in the past")
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
