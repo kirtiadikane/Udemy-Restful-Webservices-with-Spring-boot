ee69b18cb8599528ff570a0ae6a4b7a8da537126
kirti <kirtiadikane22@gmail.com>
2023-10-12 23:03:55 +0530

    User entity, H2 DB and sql data inserted

--- src/main/java/com/kirti/restwebservices/restfulwebservices/user/User.java
+++ src/main/java/com/kirti/restwebservices/restfulwebservices/user/User.java
@@ -4,12 +4,19 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name = "user_details")
 //User Bean
 public class User {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min=2, message = "Name should have atleast 2 characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into user_details(id,birth_date,name)
+values(1001, current_date(), 'kirti');
+
+insert into user_details(id,birth_date,name)
+values(1002, current_date(), 'kirtiiii');
+
+insert into user_details(id,birth_date,name)
+values(1003, current_date(), 'kirtika');
+
+insert into user_details(id,birth_date,name)
+values(1004, current_date(), 'Ranga');
+
+insert into user_details(id,birth_date,name)
+values(1005, current_date(), 'Ravi');
+
+insert into user_details(id,birth_date,name)
+values(1006, current_date(), 'Sathish');
