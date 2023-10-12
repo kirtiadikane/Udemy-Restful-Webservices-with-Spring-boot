95d7df3d3eea5bb0a8ff7cb42bd3dff42de0203a
kirti <kirtiadikane22@gmail.com>
2023-10-12 14:33:12 +0530

    static filtering API

--- src/main/java/com/kirti/restwebservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/kirti/restwebservices/restfulwebservices/filtering/FilteringController.java
@@ -3,19 +3,34 @@ package com.kirti.restwebservices.restfulwebservices.filtering;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FilteringController {
 	
-	@GetMapping("/filtering")
-	public SomeBean filtering() {
+	@GetMapping("/static-filtering")
+	public SomeBean staticfiltering() {
 		return new SomeBean("value1","value2","value3");
 	}
 	
-	@GetMapping("/filtering-list")
-	public List<SomeBean> filteringList() {
+	@GetMapping("/static-filtering-list")
+	public List<SomeBean> staticfilteringList() {
+		return Arrays.asList(new SomeBean("value1","value2","value3"),
+				new SomeBean("value4","value5","value6"));
+	}
+	
+	@GetMapping("/dynamic-filtering")
+	public MappingJacksonValue dynamicfiltering() {
+		SomeBean someBean = new SomeBean("value1","value2","value3");
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+		
+		return mappingJacksonValue;
+	}
+	
+	@GetMapping("/dynamic-filtering-list")
+	public List<SomeBean> dynamicfilteringList() {
 		return Arrays.asList(new SomeBean("value1","value2","value3"),
 				new SomeBean("value4","value5","value6"));
 	}
