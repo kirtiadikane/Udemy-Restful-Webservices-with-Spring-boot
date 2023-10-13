d26e7924d843963cf13061e815dfdd45953df3b5
kirti <kirtiadikane22@gmail.com>
2023-10-13 13:09:44 +0530

    Post entity with many to one relationship with user entity

--- src/main/java/com/kirti/restwebservices/restfulwebservices/user/User.java
+++ src/main/java/com/kirti/restwebservices/restfulwebservices/user/User.java
@@ -1,13 +1,16 @@
 package com.kirti.restwebservices.restfulwebservices.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -30,6 +33,10 @@ public class User {
 	@Past(message = "Birth date should be in the past")
 	//@JsonProperty("Birth date")
 	private LocalDate birthDate;
+	
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<Post> posts;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
 		super();
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,32 @@
 insert into user_details(id,birth_date,name)
-values(1001, current_date(), 'kirti');
+values(10001, current_date(), 'kirti');
 
 insert into user_details(id,birth_date,name)
-values(1002, current_date(), 'kirtiiii');
+values(10002, current_date(), 'kirtiiii');
 
 insert into user_details(id,birth_date,name)
-values(1003, current_date(), 'kirtika');
+values(10003, current_date(), 'kirtika');
 
 insert into user_details(id,birth_date,name)
-values(1004, current_date(), 'Ranga');
+values(10004, current_date(), 'Ranga');
 
 insert into user_details(id,birth_date,name)
-values(1005, current_date(), 'Ravi');
+values(10005, current_date(), 'Ravi');
 
 insert into user_details(id,birth_date,name)
-values(1006, current_date(), 'Sathish');
+values(10006, current_date(), 'Sathish');
+
+insert into post(id,description,user_id)
+values(20001,'I want to learn AWS', 10001);
+
+insert into post(id,description,user_id)
+values(20002,'I want to learn DevOps', 10001);
+
+insert into post(id,description,user_id)
+values(20003,'I want to Get AWS Certified', 10002);
+
+insert into post(id,description,user_id)
+values(20004,'I want to learn Multi Cloud', 10002);
+
+insert into post(id,description,user_id)
+values(20005,'Microservices', 10004);
