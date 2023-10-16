2a41b3e23c4e2afe7e340db93ce60e7c9a95294f
kirti <kirtiadikane22@gmail.com>
2023-10-16 14:42:51 +0530

    Revert "restful webservices"
    This reverts commit 54460f9c616a869ab761f2cde2d85caf5d8e8b6b.

--- restful-webservices/src/main/java/com/kirti/restwebservices/restfulwebservices/user/jpa/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kirti.restwebservices.restfulwebservices.user.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kirti.restwebservices.restfulwebservices.user.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
